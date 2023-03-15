c32bfe5382ba9a809d0198f164ef5839544f05f7
phillipe henriques <phillipe_ch@hotmail.com>
2023-03-15 00:36:06 -0300

    montando seletores de campos

--- js/principal.js
+++ js/principal.js
@@ -1,5 +1,16 @@
-document.querySelector(".titulo");
-
 var titulo = document.querySelector(".titulo");
+titulo.textContent = "Aparecida Nutricionista";
 
-titulo.textContent = "Aparecida Nutricionista"
+var paciente = document.querySelector("#primeiro-paciente"); // Seleção do paciente
+
+console.log(paciente)
+
+var tdPeso = paciente.querySelector(".info-peso");  // Seleção do peso
+var peso = tdPeso.textContent // Seleção do conteúdo do TD peso
+
+var tdAltura = paciente.querySelector(".info-altura"); // Seleção da altura
+var altura = tdAltura.textContent // Seleção do conteúdo do TD altura
+
+var IMC = peso / altura * altura
+
+// console.log(IMC)
