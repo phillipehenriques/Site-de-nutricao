7ef153f4709a0dfbf8290354b52c988b90c98ee4
phillipe henriques <phillipe_ch@hotmail.com>
2023-03-15 19:39:04 -0300

    primeira fase do código pronta

--- js/principal.js
+++ js/principal.js
@@ -1,14 +1,18 @@
 var titulo = document.querySelector(".titulo");
 titulo.textContent = "Aparecida Nutricionista";
 
-var paciente = document.getElementById("#primeiro-paciente"); // Seleção do paciente
+var paciente = document.getElementById("#primeiro-paciente"); // Acessando '#primeiro paciente' e atribuindo a variavel
 
-var tdPeso = paciente.querySelector(".info-peso");  // Seleção do peso
-var peso = tdPeso.textContent // Seleção do conteúdo do TD peso
+var tdPeso = paciente.querySelector(".info-peso");  // Acessando '.info-peso' e atribuindo a variavel
+var peso = tdPeso.textContent // Acessando o conteúdo de 'td peso' e atribuindo a variavel
 
-var tdAltura = paciente.querySelector(".info-altura"); // Seleção da altura
-var altura = tdAltura.textContent // Seleção do conteúdo do TD altura
+var tdAltura = paciente.querySelector(".info-altura"); // Acessando '.info-altura' e atribuindo a variavel
+var altura = tdAltura.textContent // Acessando o conteúdo de 'td altura' e atribuindo a variavel
 
-var IMC = peso / altura * altura
+var tdIMC = paciente.querySelector(".info-imc") // Acessando '.info-imc' e atribuindo a variavel
+var IMC = peso / (altura * altura) // Definindo a varáivel 'IMC' com base no cálculo de IMC
 
-console.log(IMC)
+tdIMC.textContent = IMC // Acessando o conteúdo de 'tdIMC' e definindo que o conteúdo deve ser a variável IMC
+
+
+
