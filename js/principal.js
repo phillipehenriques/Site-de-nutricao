ecdbccd6287cc0ddd9d29647415c92e95d06171b
phillipe henriques <phillipe_ch@hotmail.com>
2023-03-15 20:08:38 -0300

    primeira fase do código pronta

--- js/principal.js
+++ js/principal.js
@@ -10,8 +10,16 @@ var tdAltura = paciente.querySelector(".info-altura"); // Acessando '.info-altur
 var altura = tdAltura.textContent // Acessando o conteúdo de 'td altura' e atribuindo a variavel
 
 var tdIMC = paciente.querySelector(".info-imc") // Acessando '.info-imc' e atribuindo a variavel
-var IMC = peso / (altura * altura) // Definindo a varáivel 'IMC' com base no cálculo de IMC
 
+// --- Validação ---
+
+if(peso < 0  || peso > 400) {
+    alert("Tem certeza que isso está certo?")
+}
+
+// --- Definindo o cálculo de IMC e passando ele para tdIMC ---
+
+var IMC = peso / (altura * altura) // Definindo a varáivel 'IMC' com base no cálculo de IMC
 tdIMC.textContent = IMC // Acessando o conteúdo de 'tdIMC' e definindo que o conteúdo deve ser a variável IMC
 
 
