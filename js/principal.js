3c6905c0d2425d92c6ede740fabfa7bcf702b0d8
phillipe henriques <phillipe_ch@hotmail.com>
2023-03-15 18:23:31 -0300

    querySelector > getElementById

--- js/principal.js
+++ js/principal.js
@@ -1,9 +1,7 @@
 var titulo = document.querySelector(".titulo");
 titulo.textContent = "Aparecida Nutricionista";
 
-var paciente = document.querySelector("#primeiro-paciente"); // Seleção do paciente
-
-console.log(paciente)
+var paciente = document.getElementById("#primeiro-paciente"); // Seleção do paciente
 
 var tdPeso = paciente.querySelector(".info-peso");  // Seleção do peso
 var peso = tdPeso.textContent // Seleção do conteúdo do TD peso
@@ -13,4 +11,4 @@ var altura = tdAltura.textContent // Seleção do conteúdo do TD altura
 
 var IMC = peso / altura * altura
 
-// console.log(IMC)
+console.log(IMC)
